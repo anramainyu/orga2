7dfe76f03e52e24a597fdb9671921da03c7325ae
anramainyu <stephanschiefer@web.de>
2015-02-17 12:01:39 +0100

    Umbau, abstrakte Klasse abteilung in nicht abstrakte Klasse umgewandelt IT, Technik und Co gelöscht. Builder als nested class implementiert(imAufbau)

--- src/Abteilung/AbteilungKomponent.java
+++ src/Abteilung/AbteilungKomponent.java
@@ -2,6 +2,8 @@ package Abteilung;
 
 
 import Merkmale.EAbteilung;
+import Merkmale.EMitarbeiter;
+import Visitor.AbteilungsVisitor;
 
 import java.lang.String;import java.util.ArrayList;
 import java.util.Iterator;
@@ -9,31 +11,53 @@ import java.util.Iterator;
 /**
  * Created by sschiefer on 17.02.2015.
  */
-public abstract class AbteilungKomponent<E> implements IVsistable<E>
+public class AbteilungKomponent<E> implements IVsistable<E>
 {
+    EAbteilung abteilung;
+    EMitarbeiter mitarbeiter;
+
     private String name;
-    private ArrayList<E> abteilung;
-    private ArrayList mitarbeiter;
+    private ArrayList<E> abteilungListe;
+    private ArrayList mitarbeiterListe;
 
 
-    public AbteilungKomponent(String name)
+    public AbteilungKomponent(EAbteilung abteilung)
     {
-        this.abteilung = new ArrayList<E>();
-        this.name=name;
+        this.abteilungListe = new ArrayList<E>();
+        this.abteilung=abteilung;
+    }
+
+    // Der Builder - nested class
+    public static class AbteilungBuilder
+    {
+        private AbteilungKomponent abt;
+
+        //der Konstruktor des Builders - Pflichtattribute
+        private AbteilungBuilder(EAbteilung abteilung)
+        {
+            abt= new AbteilungKomponent(abteilung);
+            abt.mitarbeiter=EMitarbeiter.ABTEILUNGSLEITER;
+        }
+
     }
 
     public ArrayList<E> getAbteilungListe()
     {
-        return abteilung;
+        return abteilungListe;
     }
 
     public Iterator<E> iterator()
     {
-        return abteilung.iterator();
+        return abteilungListe.iterator();
     }
 
     public void addAbteilung(E elem)
     {
-        abteilung.add(elem);
+        abteilungListe.add(elem);
+    }
+
+    @Override
+    public void accept(IVsistable<AbteilungsVisitor> v) {
+
     }
 }
